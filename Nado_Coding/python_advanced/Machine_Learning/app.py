aa5cf1ba9e5c21c1546380c68d63d44ae396ac9c
tomato <ahsdfg30@gmail.com>
2025-02-13 19:19:15 +0900

    Feat(app): Add progress spinner, change language setting to Korean, use dotenv for importing API Key

--- Nado_Coding/python_advanced/Machine_Learning/app.py
+++ Nado_Coding/python_advanced/Machine_Learning/app.py
@@ -2,9 +2,17 @@ import pickle
 import streamlit as st
 from tmdbv3api import Movie, TMDb
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("TMDB_API")
+
 movie = Movie()
 tmdb = TMDb()
-tmdb.api_key = "b18e798ff377ef49f1c335283e7c43d6"
+tmdb.api_key = api_key
+tmdb.language = "ko-KR"
 
 
 def get_recommendations(title):
@@ -30,7 +38,7 @@ def get_recommendations(title):
         id = movies["id"].iloc[i]
         details = movie.details(id)
 
-        image_path = details["poseter_path"]
+        image_path = details["poster_path"]
         if image_path:
             image_path = "https://image.tmdb.org/t/p/w500" + image_path
         else:
@@ -50,12 +58,13 @@ st.header("Nadoflix")
 movie_list = movies["title"].values
 title = st.selectbox("Choose a movie you like", movie_list)
 if st.button("Recommend"):
-    images, titles = get_recommendations(title)
+    with st.spinner("Please wait..."):
+        images, titles = get_recommendations(title)
 
-    idx = 0
-    for i in range(0, 2):
-        cols = st.columns(5)
-        for col in cols:
-            col.image(images[idx])
-            col.write(titles[idx])
-            idx += 1
+        idx = 0
+        for i in range(0, 2):
+            cols = st.columns(5)
+            for col in cols:
+                col.image(images[idx])
+                col.write(titles[idx])
+                idx += 1
